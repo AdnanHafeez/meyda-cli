53f0a744cc003ae30a7353d0fdd4552249983eb7
Hugh Rawlinson <hughrawlinson@users.noreply.github.com>
2016-09-18 23:36:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 /* globals module */
 
-const meyda = require('meyda')
+const meyda = require('meyda');
+const fs = require('fs');
 
 module.exports = function main(filename,
                                features = meyda.featureExtractors,
                                windowingFunction='hanning',
-                               chunks){
+                               bufferSize = 1024){
 
   // Get a list of the features that have been asked for that aren't supported
   const invalidFeatures = features.reduce((accumulator,feature) => {
@@ -23,4 +24,21 @@ module.exports = function main(filename,
   }
 
   // TODO validate windowing function
+  const stream = () => {
+    if (validWavFile(filename)) {
+      return fs.createReadStream(filename);
+    }
+    return process.stdin;
+  }
+  
+  const extract = (() => {
+    meyda.bufferSize = bufferSize;
+    meyda.windowingFunction = windowingFunction;
+    return (chunk) => meyda.extract(features, chunk);
+  })();
+  
+  const chunkStream = new ChunkStream(extract, bufferSize);
+  
+  stream.pipe(chunkStream);
+  chunkStream.pipe(process.stdout);
 }
